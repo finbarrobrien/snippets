6cfc88aa4c2751cdd7fd0d35d2d50ad3f700eb94
finbarrobrien <finbarrobrien@users.noreply.github.com>
2017-03-06 10:20:06 +0000

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,15 @@
+let arr = [6,4,8,1,2,9,7,3,5];
+let swap = true;
+console.log(arr.toString());
+for(let i = 1; i < arr.length; i++)
+{
+	console.log(arr[i]);
+	let j = i - 1;
+    let m = i;
+    while ( j >= 0 && arr[j] > arr[m]){
+    	arr.splice(j, 0, arr.splice(m,1));
+        m = j;
+        j--;
+    }
+  	console.log(arr.toString());
+}
