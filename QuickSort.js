461575143dee6eaeba3a5f30320dbaedbec0df1d
finbarrobrien <finbarrobrien@users.noreply.github.com>
2017-03-07 20:28:54 +0000

    Create QuickSort.js

---
+++ QuickSort.js
@@ -0,0 +1,22 @@
+let nums = [33,554,657,912,121,221,6654,448,4,87,10,101,785,15,400,365];
+
+const partitionAndRecurse = (array, startIdx, endIdx) => {
+    let p=startIdx;
+	if(startIdx != endIdx){
+    	for(let i = startIdx; i < endIdx; i++){
+        	if(array[i] < array[p] && i > p){
+            	// shift array[i] to left of pivot and increment pivot
+            	array.splice(p,0,array.splice(i,1)[0]);
+                p++; i--;
+            }
+        }
+        // left of pivot
+        partitionAndRecurse(nums, startIdx, p);
+        // right of pivot
+        p++;
+        partitionAndRecurse(nums, p, endIdx);
+    }
+};
+
+partitionAndRecurse(nums, 0, nums.length);
+console.log(nums.toString());
